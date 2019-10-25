5e2ab6adba749d75c1ee9f156179b54dd2528ee5
saghari97 <41893802+saghari97@users.noreply.github.com>
2019-10-25 22:09:33 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const whiskers = require('whiskers');
 const app=expree();
 const port = 3000;
-var features = [];
+var arrs = [];
 var areas = fs.readFilesSync('./sample-data.json');
 var InternetEng = JSON.parse(areas.toString());
+InternetEng.arrs.forEach(function (arr) {
+  arrs.push(feature);
+
+});
 console.log('--------------------------- NEW APP ')
 app.use('/' , (req, res, next)=>{
     console.log('Middleware Called!')
