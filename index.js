ed9323c549a09fd7078e6586f235dd26652c3a30
saghari97 <41893802+saghari97@users.noreply.github.com>
2019-10-25 22:04:33 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 const express = require('express');
+const whiskers = require('whiskers');
+const app=expree();
+const port = 3000;
 var areas = fs.readFilesSync('./sample-data.json');
 var InternetEng = JSON.parse(areas.toString());
-
+console.log('--------------------------- NEW APP ')
+app.use('/' , (req, res, next)=>{
+    console.log('Middleware Called!')
+    next();
+});
+app.get('/', (req, res) => {
+    console.log(req.query)
+  
