daa81cf73760f19ceff78494a0c5e5801f31c3ef
saghari97 <41893802+saghari97@users.noreply.github.com>
2019-10-25 23:22:51 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 const express = require('express');
 const whiskers = require('whiskers');
-const app=expree();
+const app=express();
 const port = 3000;
 var pointI = require ('point-in-polygon');
 var arrs = [];
 var areas = fs.readFilesSync('./sample-data.json');
-var InternetEng = JSON.parse(areas.toString());
+var InternetEng = JSON.parse(areas);
 InternetEng.arrs.forEach(function (arr) {
   arrs.push(feature);
 
 });
+app.use(express.JSON());
+app.get('/gis/testpoint',(req,res)=>{
+var result = {polygons:[]};
+try{
+ var pointII = [parse float(req.query.lat)
+ features.foreach(function(feature){
+  feature.geometry.coordinates.foreach(function(coordinates){
+   if(pointI(pointII,coordinates))
+    result.polygons.push(feature
+/////////
 console.log('--------------------------- NEW APP ')
 app.use('/' , (req, res, next)=>{
     console.log('Middleware Called!')
